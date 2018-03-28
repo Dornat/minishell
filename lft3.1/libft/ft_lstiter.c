b82dd600204fe5a0aa8f90c354d069c90a3d2c3f
Dmytro POLOSUKHIN <dpolosuk@e1r2p7.unit.ua>
2018-03-28 16:47:36 +0300

    hope it won't segfault

--- lft3.1/libft/ft_lstiter.c
+++ lft3.1/libft/ft_lstiter.c
@@ -6,7 +6,7 @@
 /*   By: dpolosuk <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/09 16:12:04 by dpolosuk          #+#    #+#             */
-/*   Updated: 2017/12/09 12:24:16 by dpolosuk         ###   ########.fr       */
+/*   Updated: 2018/03/28 16:41:36 by dpolosuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,4 +22,21 @@ void	ft_lstiter(t_list *lst, void (*f)(t_list *elem))
 		(*f)(tmp);
 		tmp = tmp->next;
 	}
+}
+
+int		ft_lstlen(t_list *lst)
+{
+	t_list		*p;
+	int			i;
+
+	p = lst;
+	i = 0;
+	if (lst == NULL)
+		return (0);
+	while (p)
+	{
+		p = p->next;
+		i++;
+	}
+	return (i);
 }
--- src/exec_prog.c
+++ src/exec_prog.c
@@ -6,26 +6,51 @@
 /*   By: dpolosuk <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/17 16:10:44 by dpolosuk          #+#    #+#             */
-/*   Updated: 2018/03/26 10:32:36 by dpolosuk         ###   ########.fr       */
+/*   Updated: 2018/03/28 16:41:49 by dpolosuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <msh.h>
 
-void	exec_prog(t_cli *cli)
+static char		**environ_from_t_list(t_list *env)
+{
+	char	**res;
+	int		len;
+	int		i;
+	t_list	*p;
+
+	len = ft_lstlen(env);
+	i = 0;
+	p = env;
+	if ((res = (char**)malloc(sizeof(char*) * (len + 1))) == NULL)
+		return (NULL);
+	while (i < len)
+	{
+		res[i] = ft_strdup(p->content);
+		i++;
+		p = p->next;
+	}
+	res[i] = NULL;
+	return (res);
+}
+
+void			exec_prog(t_cli *cli)
 {
 	extern pid_t			g_pid;
 	extern struct termios	g_raw;
 	extern char				**environ;
+	char					**env;
 
+	env = environ_from_t_list(ENV);
 	if ((g_pid = fork()) < 0)
 		ft_putendl_fd("Fork returned error!!!???", 2);
 	else if (!g_pid)
 	{
 		disable_raw_mode();
-		execve(PTH, ACMD, environ);
+		execve(PTH, ACMD, env);
 	}
 	wait(NULL);
 	g_pid = 0;
 	tcsetattr(0, TCSAFLUSH, &g_raw);
+	free_double_ptr(&env);
 }
